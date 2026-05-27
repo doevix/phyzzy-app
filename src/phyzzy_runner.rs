486d025a2bd654530859c3a06c8ebe274963bf15
Laura Fox <laura.doevix@gmail.com>
2026-05-26 22:29:38 -0600

    separate into multiple files

---
+++ src/phyzzy_runner.rs
@@ -0,0 +1,198 @@
+use phyzzy_rs::{ self, V2D };
+use eframe::{ egui::{ self, Color32, Pos2, Rect, Sense, Vec2, Response }, epaint::CornerRadiusF32 };
+use std::time::Instant;
+
+use crate::phyzzy_sim::PhyzzySimulator;
+
+pub struct PhyzzyApp {
+    pub phz: PhyzzySimulator,
+    pub pointer_pos: Option<Pos2>,
+    pub pointer_interact_pos: Option<Pos2>,
+    pub pointer_drag_delta: Vec2,
+    pub drag_vel: V2D,
+    pub mass_idx: Option<usize>,
+    pub held_idx: Option<usize>,
+}
+
+
+impl PhyzzyApp {
+    pub fn new(_cc: &eframe::CreationContext<'_>, phz: PhyzzySimulator) -> Self {
+        Self {
+            phz,
+            pointer_pos: None,
+            pointer_interact_pos: None,
+            pointer_drag_delta: Vec2::new(0.0, 0.0),
+            drag_vel: V2D::null(),
+            mass_idx: None,
+            held_idx: None,
+        }
+    }
+}
+
+impl PhyzzyApp {
+    pub fn user_interact(&mut self, response: &Response) {
+        match response.hover_pos() {
+            Some(pos) => {
+                self.pointer_pos = Some(pos);
+                let mut m_idx: Option<usize> = None;
+                for (idx, mass) in self.phz.model.get_masses().iter().enumerate() {
+                    let bound_rad = ((mass.r * self.phz.scaling) + 10.0) as f32;
+                    let mass_pos = self.phz.world_to_panel(&mass.p_i);
+
+                    if (pos - mass_pos).length() < bound_rad {
+                        m_idx = Some(idx);
+                        break;
+                    }
+                }
+
+                self.mass_idx = m_idx;
+                m_idx
+            },
+            None => {
+                self.pointer_pos = None;
+                self.mass_idx = None;
+                None
+            }
+        };
+
+        match response.interact_pointer_pos() {
+            Some(pos) => {
+                self.pointer_interact_pos = Some(pos);
+
+                // Find the mass that's being clicked on.
+                let mut m_idx: Option<usize> = None;
+                for (idx, mass) in self.phz.model.get_masses().iter().enumerate() {
+                    let bound_rad = ((mass.r * self.phz.scaling) + 10.0) as f32;
+                    let mass_pos = self.phz.world_to_panel(&mass.p_i);
+
+                    if (pos - mass_pos).length() < bound_rad {
+                        m_idx = Some(idx);
+                        break;
+                    }
+                }
+
+                // If a mass was found for interaction.
+                if let Some(idx) = m_idx {
+                    // If the mass was already being used for interaction.
+                    match self.held_idx {
+                        None => {
+                            self.held_idx = m_idx;
+                            // self.drag_vel = V2D::null();
+                            self.phz.model.hold_mass(idx);
+                        },
+                        Some(_) => {},
+                    }
+                }
+            },
+            None => {
+                self.pointer_interact_pos = None;
+                if let Some(idx) = self.held_idx {
+                    self.phz.model.release_mass(idx);
+                    self.held_idx = None;
+                    self.drag_vel = V2D::null();
+                }
+            }
+        };
+
+        self.pointer_drag_delta = response.drag_delta();
+
+        if response.dragged() {
+            if let (Some(idx), Some(cursor)) = (self.held_idx, response.interact_pointer_pos()) {
+                let p_x = (cursor.x as f64 - self.phz.screen_rect.min.x as f64) / self.phz.scaling;
+                let p_y = (self.phz.screen_rect.max.y as f64 - cursor.y as f64) / self.phz.scaling;
+                let target = V2D::new(p_x, p_y);
+
+                self.phz.model.set_mass_pos(idx, target);
+
+                let drag_delta = response.drag_delta();
+                let frame_vel = V2D::new(
+                    drag_delta.x as f64 / self.phz.scaling,
+                    -drag_delta.y as f64 / self.phz.scaling
+                ) / self.phz.last_frame;
+
+                // Since the drag velocity gets lost on release, hold on to the last non-zero velocity.
+                self.drag_vel = if frame_vel.mag2() > 0.0 { frame_vel } else { self.drag_vel };
+            }
+        }
+
+        if response.drag_stopped() {
+            if let Some(idx) = self.held_idx {
+                self.phz.model.set_mass_vel(idx, self.drag_vel, self.phz.dt);
+                self.phz.model.release_mass(idx);
+                self.held_idx = None;
+                self.drag_vel = V2D::null();
+            }
+        }
+    }
+}
+
+
+impl eframe::App for PhyzzyApp {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        egui::Panel::left("options_panel")
+        .resizable(true)
+        .min_size(150.0)
+        .max_size(500.0)
+        .show_inside(ui, |ui| {
+            ui.heading(&self.phz.model_meta.name);
+            let creator_string = format!("by {}", self.phz.model_meta.creator);
+            ui.label(creator_string);
+            if ui.button("Pause").clicked() {
+                self.phz.paused = !self.phz.paused;
+            }
+            if ui.button("change wave dir").clicked() {
+                self.phz.model.wave_speed *= -1.0;
+            }
+
+            let mousing = format!("{:?}, {:?}", self.pointer_pos, self.pointer_interact_pos);
+            let mass_found = format!("mass found: {:?}", self.mass_idx);
+            let drag_speed = format!("Dragging at {:?} pps", self.pointer_drag_delta);
+            ui.label(mousing);
+            ui.label(drag_speed);
+            ui.label(mass_found);
+        });
+        egui::CentralPanel::default().show_inside(ui, |ui| {
+            // Get time passed.
+            let t_elapsed = self.phz.t_now.elapsed();
+            self.phz.last_frame = t_elapsed.as_secs_f64();
+            self.phz.t_now = Instant::now();
+
+            // Setup painter.
+            let full_area = ui.max_rect();
+            let (scaled_area, scale) = self.phz.area_to_rect(full_area);
+            self.phz.scaling = scale as f64;
+
+            // Center the viewer.
+            let center_offset = Vec2::new(
+                (full_area.width() - scaled_area.x) * 0.5,
+                (full_area.height() - scaled_area.y) * 0.5,
+            );
+            let centered_min = full_area.min + center_offset;
+            let centered_rect = Rect::from_min_size(centered_min, scaled_area);
+            let painter = ui.painter_at(centered_rect);
+            self.phz.screen_rect = centered_rect;
+
+            let response = ui.allocate_rect(centered_rect, Sense::click_and_drag());
+
+            // User interaction.
+            self.user_interact(&response);
+
+            // Update model.
+            let mut acc = self.phz.last_frame;
+            while acc >= self.phz.dt {
+                self.phz.model.step(self.phz.dt, &self.phz.world, &self.phz.world_cfg, self.phz.paused);
+                acc -= self.phz.dt;
+            }
+            let alpha = acc / self.phz.dt;
+
+            // Draw the background.
+            ui.request_repaint();
+            let no_radius = CornerRadiusF32::same(0.0);
+            ui.painter().rect_filled(full_area, no_radius, Color32::from_gray(0));
+            painter.rect_filled(self.phz.screen_rect, no_radius, Color32::from_gray(16));
+
+            // Draw the model.
+            self.phz.draw_model(&painter, alpha, self.mass_idx);
+        });
+    }
+}
---
+++ src/phyzzy_sim.rs
@@ -0,0 +1,132 @@
+use phyzzy_rs::{ self, Model, V2D, World, WorldConfig };
+use eframe::egui::{ Color32, Painter, Pos2, Rect, Stroke, Vec2, };
+use std::time::Instant;
+
+use crate::phyzzy_io::PhyzzyMeta;
+
+pub struct PhyzzySimulator {
+    pub model_meta: PhyzzyMeta,
+    pub world: World,
+    pub world_cfg: WorldConfig,
+    pub model: Model,
+    pub scaling: f64,
+    pub dt: f64,
+    pub last_frame: f64,
+    pub t_now: Instant,
+    pub screen_rect: Rect,
+    pub paused: bool,
+}
+
+impl PhyzzySimulator {
+    pub fn new() -> Self {
+        Self {
+            dt: 0.001,
+            last_frame: 0.0,
+            scaling: 0.0,
+            world: World::new(&V2D::new(12.0, 7.5)),
+            world_cfg: WorldConfig { gravity: V2D::new(0.0, -9.81), drag: 0.0 },
+            model: Model::new(5.0, 1.0),
+            model_meta: PhyzzyMeta {
+                name: "No name".to_string(),
+                creator: "anonymous".to_string(),
+                created: "no date".to_string(),
+            },
+            t_now: Instant::now(),
+            screen_rect: Rect {
+                min: Pos2 { x: 0.0, y: 0.0 },
+                max: Pos2 { x: 0.0, y: 0.0 },
+            },
+            paused: false,
+        }
+    }
+
+    // Transforms vector to window coordinates. Requires conversion to Vec2
+    pub fn tf_coord(&self, phz_coord: &V2D) -> V2D {
+        // Function arranged for clarity on transformation matrix being used.
+        phz_coord.tf_fit(
+            self.scaling,                   self.screen_rect.max.y as f64,
+            self.screen_rect.min.x as f64, -self.scaling
+        )
+    }
+
+    pub fn world_to_panel(&self, phz_coord: &V2D) -> Pos2{
+        let tf = self.tf_coord(phz_coord);
+        Pos2::new(tf.x as f32, tf.y as f32)
+    }
+
+    // Set the area. Changes the scale.
+    pub fn area_to_rect(&self, rect: Rect) -> (Vec2, f32) {
+        let rect_sz = rect.size();
+        let world_sz = Vec2::new(self.world.area_sz.x as f32, self.world.area_sz.y as f32);
+
+        if world_sz.x > world_sz.y {
+            let scale = rect_sz.x / world_sz.x;
+            // Clamp vertical size if it gets bigger than the window's.
+            if world_sz.y * scale > rect_sz.y {
+                let s = rect_sz.y / world_sz.y;
+                return (Vec2::new(world_sz.x * s, rect_sz.y), s);
+            }
+
+            (Vec2::new(rect_sz.x, world_sz.y * scale), scale)
+        } else {
+            let scale = rect_sz.y / self.world.area_sz.y as f32;
+            // Clamp horizontal size if it gets bigger than the window's.
+            if world_sz.x * scale > rect_sz.x {
+                let s = rect_sz.x / world_sz.x;
+                return (Vec2::new(rect_sz.x, world_sz.y * s), s);
+            }
+
+            (Vec2::new(world_sz.x * scale, rect_sz.y), scale)
+
+        }
+
+    }
+
+
+
+    pub fn draw_model(&self, painter: &Painter, alpha: f64, hover_idx: Option<usize>) {
+        let color = Color32::from_gray(128);
+        let stroke = Stroke::new(1.0, color);
+
+        // Draw springs
+        for spring in self.model.get_springs() {
+            let p_a = self.world_to_panel(&self.model.get_mass(spring.get_ma()).p_i);
+            let p_b = self.world_to_panel(&self.model.get_mass(spring.get_mb()).p_i);
+
+            painter.line_segment([p_a, p_b], stroke);
+        }
+
+        // Draw masses
+        let mass_color = Color32::from_hex("#1DB322").unwrap();
+        for (idx, mass) in self.model.get_masses().iter().enumerate() {
+
+            // Final frame interpolation. Reference: https://www.gafferongames.com/post/fix_your_timestep/
+            let p_render = mass.p_i * alpha + mass.p_o * (1.0 - alpha);
+            let pos = self.world_to_panel(&p_render);
+
+            let rad = (mass.r * self.scaling)  as f32;
+
+            painter.circle_filled(pos, rad, mass_color);
+
+            match hover_idx {
+                None => {},
+                Some(h_idx) => {
+                    if idx == h_idx {
+                        let highlight_color = Color32::from_gray(255);
+                        let highlight_stroke = Stroke::new(1.0, highlight_color);
+                        let highlight_rad = 5.0;
+                        painter.circle_stroke(pos, rad + highlight_rad, highlight_stroke);
+                    }
+                },
+            }
+        }
+    }
+
+    pub fn _draw_world(&self, _painter: &Painter, _world: &World) {
+        let color = Color32::from_gray(128);
+        let _stroke = Stroke::new(1.0, color);
+
+
+    }
+
+}
